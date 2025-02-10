52ac08d3b25d655872e184769158c2f4928821e1
imvenx <imvenx@proton.me>
2025-02-10 10:55:48 +0100

    fix hud show collected gems on load level, fix delete all storage function

--- singletons/Global.gd
+++ singletons/Global.gd
@@ -17,46 +17,29 @@ var default_collected_gems = [
 	[], [], [], [], [], [], [], [], [], [], [], [], [], [],
 ]
 
-@onready var collected_gems: Array = getItem('collected_gems', default_collected_gems)
+@onready var collected_gems: Array = loadItem('collected_gems', default_collected_gems)
 func set_collected_gems(level: int, gem: String):
 	collected_gems[level - 1].push_front(gem)
-	setItem('collected_gems', collected_gems)
+	saveItem('collected_gems', collected_gems)
 
 
-@onready var last_unlocked_level = getItem('last_unlocked_level', 1)
+const defaultUnlockedLevel = 1
+@onready var last_unlocked_level = loadItem('last_unlocked_level', defaultUnlockedLevel)
 func set_last_unlocked_level(val: int):
 	last_unlocked_level = val
-	setItem('last_unlocked_level', val)
+	saveItem('last_unlocked_level', val)
 	#save("last_unlocked_level", val)
 	
 
 var current_level = 0
 
 func _ready():
-	var path = "user://save.json"
-
-	#setItem('w', 'waa')
-	print(getItem('w'))
-	#var q = getItem('last_unlocked_level', 1)
-	#print(q)
-	#s('aa', 'aa')
-	#print(l('aa'))
-	# Save data
-	#var data_to_save = {"asd": "hello world"}
-	#var file = FileAccess.open(path, FileAccess.WRITE)
-	#file.store_string(JSON.stringify(data_to_save))
-	#file.close()
-
-	# Load data
-	#if FileAccess.file_exists(path):
-		#var _file = FileAccess.open(path, FileAccess.READ)
-		#var loaded_data = JSON.parse_string(_file.get_as_text())
-		#_file.close()
-		#print(loaded_data.get("asd", "default_value"))  # Should print: "hello world"
+	#deleteAllStorage()
+	pass
 
 var path = "user://save.json"
 
-func setItem(key: String, val):
+func saveItem(key: String, val):
 	# Load existing data to avoid overwriting other keys
 	var data_to_save = {}
 
@@ -73,12 +56,12 @@ func setItem(key: String, val):
 	file.store_string(JSON.stringify(data_to_save))
 	file.close()
 
-func getItem(key: String, default_value = null):
+func loadItem(key: String, default_value = null):
 	if FileAccess.file_exists(path):
 		var file = FileAccess.open(path, FileAccess.READ)
 		var loaded_data = JSON.parse_string(file.get_as_text()) as Dictionary
 		file.close()
-		return loaded_data.get(key, default_value)
+		var val = loaded_data.get(key, default_value)
 
 	return default_value
 
@@ -94,5 +77,5 @@ func deleteAllStorage():
 	#last_unlocked_level = 1
 
 	# Re-save defaults to prevent errors when reloading the game
-	setItem("collected_gems", null)
-	setItem("last_unlocked_level", null)
+	saveItem("collected_gems", default_collected_gems)
+	saveItem("last_unlocked_level", defaultUnlockedLevel)
