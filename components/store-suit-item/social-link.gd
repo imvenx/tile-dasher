04bf95b597e0ef78781ad65a69a643da6309353a
imvenx <imvenx@proton.me>
2025-02-17 15:43:44 +0100

    add discord link, fix game music and sound effects panning when they should not

--- components/store-suit-item/social-link.gd
+++ components/store-suit-item/social-link.gd
@@ -6,25 +6,25 @@ func _ready() -> void:
 	$tooltip.modulate.a = 0
 	$Area2D.connect('body_entered', onBodyEnter)
 	$Area2D.connect('body_exited', onBodyExit)
-	$RichTextLabel.text = name.capitalize() + ' Link'
+	$RichTextLabel.text = name.capitalize()
 
 func onBodyEnter(body: Node2D) -> void:
 	if body.is_in_group("player"): 
 		isPlayerInside = true
-		body.changeSuit(name)
-		$"change-clothes-sound".play()
+		$"SfxrStreamPlayer".play()
 		
-		if not Global.unlockedSuits.has(name):
-			await get_tree().create_timer(.5).timeout
-			var tween = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
-			tween.tween_property($tooltip, "modulate:a", 1.0, 0.3) 
+		var tween = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+		tween.tween_property($tooltip, "modulate:a", 1.0, 0.3) 
+		#if not Global.unlockedSuits.has(name):
+			#await get_tree().create_timer(.5).timeout
 
 func onBodyExit(body: Node2D) -> void:
 	if body.is_in_group("player"): 
 		isPlayerInside = false
 		
 		if Global.unlockedSuits.has(name):
-			Global.setCurrentSuit(name)
+			pass
+			#Global.setCurrentSuit(name)
 		else:
 			var tween = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
 			tween.tween_property($tooltip, "modulate:a", 0.0, 0.3)
